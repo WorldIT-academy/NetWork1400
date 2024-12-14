f7521c33f25c453d88d67384a2c4665e0992297f
Dima <you@example.com>
2024-12-14 14:30:42 +0100

    create registration page

---
+++ social_network/user_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+
+def render_registration(request):
+    if request.method == 'POST':
+        print(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        User.objects.create_user(username= username, password= password)
+    return render(request=request, template_name= "user_app/registration.html")
+
+
