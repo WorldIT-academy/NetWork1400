2a8e3c94444c513d42db7ade2bcff24785f1cd7e
Dima <you@example.com>
2025-02-15 14:25:05 +0100

    Create django form

--- social_network/post_app/urls.py
+++ social_network/post_app/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('all_posts/', render_all_posts),
+    path('all_posts/', render_all_posts, name = 'all_posts'),
     path('create_post/', render_create_post),
 ]
--- social_network/post_app/views.py
+++ social_network/post_app/views.py
@@ -1,43 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Tag
 from django.core.files.storage import FileSystemStorage
 from user_app.models import Profile
 import os
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
 
 
 def render_all_posts(request):
     all_posts = Post.objects.all()
     return render(request,"post_app/all_posts.html",context={"all_posts":all_posts})   
 
+# Декоратор перевіряє, чи залогінився користувач,
+# Якщо ні - його перекидає на сторінку логіну, шлях до йкої прописано у констатні LOGIN_URL у settings.py
+@login_required
 def render_create_post(request):
-    # Отримуємо усі теги з БД
-    all_tags = Tag.objects.all()
-    # Розміщуємо теги у контекст для передачі у HTML-шаблон
-    context = {'tags': all_tags}
     # Якщо надсилається тип запиту POST, тобто користувач надсилає форму
     if request.method == "POST":
-        # Отримуємо заголовок з HTML форми
-        title = request.POST.get("title")
-        # Отримуємо контент з HTML форми
-        content = request.POST.get("content")
+        # Створюємо об'єкт форми та наповнюємо її даними та файлами, які надсилають користувачі через форму
+        form = PostForm(request.POST, request.FILES)
+        # Якщо надіслані дані є коректними (якщо пройшла валідація)
+        if form.is_valid():
+            post = Post.objects.create(
+                title = form.cleaned_data.get('title'),
+                content = form.cleaned_data.get('content'),
+                image = form.cleaned_data.get('image'),
+                author = Profile.objects.get(user= request.user)
+            )
+            post.tags.set(form.cleaned_data.get('tags'))
+            post.save()
 
-        # Отримуємо зображення з форми
-        image = request.FILES.get("image")
-        # Створоюємо (об'єднуємо) шлях для зображення
-        image_path = os.path.join("images", "posts", image.name)
-        # Створюємо об'єкт файлової системи для збереження зображення
-        file_system = FileSystemStorage()
-        # Зберігаємо зображення за вказаним шляхом
-        file_system.save(image_path, image)
-
-        # Отримуємо об'єкт користувача, що залогінився
-        user = request.user
-        # Отримуємо профіль користувача, що залогінився
-        profile = Profile.objects.get(user=user)
-
-        # Отримуємо теги, які обрав користувач у формі
-        tags = request.POST.getlist("tags")
+            return redirect('all_posts')
+    else:
+        # Створюємо порожню форму для відображення на сторінці
+        form = PostForm()
         
-        
-   
-    return render(request, "post_app/create_post.html", context=context)
+    return render(request, "post_app/create_post.html", context={'form': form})
